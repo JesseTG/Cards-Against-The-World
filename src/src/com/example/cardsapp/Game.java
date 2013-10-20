18e83167a97222b47f9c3b52f19a0c4f2f07974e
ang6190 <allison.ng@stonybrook.edu>
2013-10-19 21:45:37 -0400

    created Game Class

---
+++ src/src/com/example/cardsapp/Game.java
@@ -0,0 +1,30 @@
+package com.example.cardsapp;
+
+public class Game
+{
+	/**
+	 * Sends the player information so that the server would push player in a pre-existing game
+	 * @param player User that wants to join a pre-existing game
+	 */
+	public static void join(User player)
+	{
+		String paramString = 
+				"activity=join"+
+				"user="+player.getUserName()+
+				"ip="+player.getIP();
+		send(paramString);
+	}
+	
+	/**
+	 * Sends the player information so that the server would create a new game and push the player in it
+	 * @param player User that wants to create a new game
+	 */
+	public static void create(User player)
+	{
+		String paramString = 
+				"activity=create"+
+				"user="+player.getUserName()+
+				"id="+player.getIP();
+		send(paramString);
+	}
+}
